a145d003d4e807dfb086e4be28ab89ac41a0d834
Frank-ds <126402108+Frank-ds@users.noreply.github.com>
2023-04-13 14:54:13 +0200

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,8 +1,42 @@
 import pandas as pd
 import pickle
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from scikit-learn.cluster import KMeans
 
-conv_path = r"c:\users\frank\Documents\DME\Eindopdracht\data\converted"
-with open(conv_path + "\E214_230129_.pkl", "rb") as f:
+conv_path = r"c:\users\frank\Documents\DME\Eindopdracht_without_raw\data\Processed"
+with open(conv_path + "\E214_A.pkl", "rb") as f:
     df = pickle.load(f)
 
-df.info()
+# g = sns.FacetGrid(df, col="Truck_State")
+# g.map(sns.scatterplot, "engine_speed", "EnginePower", alpha=0.7)
+# plt.show()
+
+print(np.sum(df.No_State))
+print(np.sum(df.working))
+print(len(df.engine_speed))
+
+
+# Load the dataframepoetry add
+df = pd.read_csv("data.csv")
+
+# Determine the number of clusters
+k = ...
+
+# Extract the signals data from the dataframe
+signals_data = df.iloc[:, 0:50].values
+
+# Initialize the k-means object with the number of clusters
+kmeans = KMeans(n_clusters=k)
+
+# Fit the k-means object to the signals data
+kmeans.fit(signals_data)
+
+# Get the cluster assignments for each data point
+labels = kmeans.labels_
+
+# Add the cluster assignments to the original dataframe
+df["Cluster"] = labels
+
+
