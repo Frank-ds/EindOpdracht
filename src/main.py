3310387182975309847d8f599ddd984493abedc0
Frank-ds <126402108+Frank-ds@users.noreply.github.com>
2023-04-13 15:19:18 +0200

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,20 @@
+from settings import settings
+import pathlib as Path
+import pandas as pd
+import pickle
+import load_data
+import preprocess
+
+DATA_RAW_DIR = settings.data_raw_dir
+DATA_LOADED_DIR = settings.loaded_path
+COMBI_FILE = preprocess.combi_file_name
+
+load_data.load_input(DATA_RAW_DIR, DATA_LOADED_DIR)
+
+preprocess.merge_pickles(DATA_LOADED_DIR, COMBI_FILE)
+
+preprocess.preprocess()
+
+
+if __name__ == "__main__":
+    print("Main done")
